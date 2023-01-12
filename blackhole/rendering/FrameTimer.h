720c53339008b8a0dd438837e3918a9839fe2391
tartarus-git <niktedig@gmail.com>
2023-01-12 07:58:29 +0100

    Fixed a compile error and added a comment.

--- blackhole/rendering/FrameTimer.h
+++ blackhole/rendering/FrameTimer.h
@@ -6,18 +6,19 @@
 
 class FrameTimer {
 public:
-	std::chrono::duration<std::chrono::nanoseconds> max_frame_duration_in_nanoseconds;
+	std::chrono::nanoseconds max_frame_duration_in_nanoseconds;
 	std::chrono::time_point<std::chrono::high_resolution_clock> frame_start;
 
 	constexpr FrameTimer(uint16_t max_FPS) noexcept {
 		constexpr uint64_t nanoseconds_per_second = (uint64_t)1000 * (uint64_t)1000 * (uint64_t)1000;
-		max_frame_duration_in_nanoseconds = (std::chrono::duration<std::chrono::nanoseconds>)(std::chrono::nanoseconds)(nanoseconds_per_second / max_FPS);
+		// NOTE: std::chrono::nanoseconds contains a signed representation of nanoseconds, just so you know.
+		max_frame_duration_in_nanoseconds = (std::chrono::nanoseconds)(nanoseconds_per_second / max_FPS);
 	}
 
 	void mark_frame_start() noexcept { frame_start = std::chrono::high_resolution_clock::now(); }
 
 	void wait_if_needed() const noexcept {
-		std::chrono::duration<std::chrono::nanoseconds> frame_duration = (std::chrono::duration<std::chrono::nanoseconds>)(std::chrono::high_resolution_clock::now() - frame_start);
+		std::chrono::nanoseconds frame_duration = (std::chrono::nanoseconds)(std::chrono::high_resolution_clock::now() - frame_start);
 		if (frame_duration < max_frame_duration_in_nanoseconds) {
 			std::this_thread::sleep_for(max_frame_duration_in_nanoseconds - frame_duration);
 		}
